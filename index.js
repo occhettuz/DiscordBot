daa13b7b5f33e481ff6fd6c07218748fdfca5772
occhettuz <61429356+occhettuz@users.noreply.github.com>
2021-05-20 16:27:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ bot.on('message', (message) => {
 		message.channel.send('ORARIO ATTUALE :alarm_clock:: ' + ora + ':' + minuto);
     } if (message.content == 'mi senti?') {
         console.log(message.author.id)
-        if (message.author.id == '493453529109889025' || message.author.id == '434435376250224650' || message.author.id == '691221421111902209') { //find isma code
+        if (message.author.id == '493453529109889025' || message.author.id == '434435376250224650' || message.author.id == '691221421111902209' || message.author.id = '705091422432395326') { //find isma code
             message.channel.send('isma gay')
         }
     }
@@ -26,4 +26,5 @@ CODICI PERSONE DS:
 zambe: 493453529109889025
 ceria: 691221421111902209
 mora: 434435376250224650
+occhetta: 705091422432395326
 */
